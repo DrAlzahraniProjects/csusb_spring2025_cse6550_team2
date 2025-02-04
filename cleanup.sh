1139a4363d221ae6216169d1d17c0bd4f245715f
Nathan K. <197807320+N-Kel@users.noreply.github.com>
2025-02-04 10:45:49 -0800

    Fix cleanup bug

--- cleanup.sh
+++ cleanup.sh
@@ -4,8 +4,8 @@ PORT="2502"
 
 # Check if any Docker containers for "team2s25-app" exist. Remove them if so.
 echo "Cleaning up previous app instances..."
-docker ps -a -q --filter "ancestor=%APP_NAME%" | xargs -r docker rm -f > /dev/null 2>&1
+docker ps -a -q --filter "ancestor=$APP_NAME" | xargs -r docker rm -f > /dev/null 2>&1
 # Check if any Docker images named "team2s25-app" exist. Remove them if so.
-docker images -q %APP_NAME% | xargs -r docker rmi -f > /dev/null 2>&1
+docker images -q $APP_NAME | xargs -r docker rmi -f > /dev/null 2>&1
 
 # TODO: Close port
