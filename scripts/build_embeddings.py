e2f47659f397642dd9fb8bbcadd1ddd14ab66943
Nathan K. <197807320+N-Kel@users.noreply.github.com>
2025-04-14 08:50:38 -0700

    Fix bug (volume was not attaching) + switch embedding model

--- scripts/build_embeddings.py
+++ scripts/build_embeddings.py
@@ -1,18 +1,26 @@
 import json
-from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.embeddings import FastEmbedEmbeddings
 from langchain_community.vectorstores import FAISS
 
 def main():
     # 1. Load the JSON data
     try:
-        with open("../data/output.json", "r") as f: data: list[dict[str, str]] = json.load(f)
+        with open("../data/output.json", "r") as f: data: list[dict[str, str | list[str]]] = json.load(f)
     except Exception as e:
         print(f"Error loading JSON data: {e}")
         return
 
     # Extract all text segments from the JSON file.
-    # segments: list[str] = ["".join(s for s in item.get("segments", [])) for item in data]
-    segments: list[str] = [segment for page in data for segment in page.get("segments", [])]
+    # segments: list[str] = []
+    # urls: list[dict[str, str]] = []
+    # for page in data:
+    #     currentSegments: list[str] = page.get("segments", [])
+    #     segments += currentSegments
+    #     currentUrl: str = page.get("url", "")
+    #     urls += [{"url": currentUrl} for _ in currentSegments]
+
+    segments = [segment for page in data for segment in page.get("segments", [])]
+    urls = [{"url": page.get("url", "")} for page in data for _ in page.get("segments", [])]
     print(f"Total segments loaded: {len(segments)}")
 
     if not segments:
@@ -20,11 +28,11 @@ def main():
         return
 
     # 2. Generate embeddings using a pre-trained SentenceTransformer model.
-    embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+    embeddings = FastEmbedEmbeddings()
     print("Embeddings generated.")
 
     # 3. Build a FAISS index.
-    vectorstore = FAISS.from_texts(segments, embeddings)
+    vectorstore = FAISS.from_texts(segments, embeddings, urls)
     print(f"FAISS index built.")
 
     # 4. Save the FAISS index to disk.
